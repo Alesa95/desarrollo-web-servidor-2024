3c4b829427a78bf30080089285a9093cb0a5f98a
Alesa95 <alejandra_santiago_95@hotmail.com>
2024-10-09 12:34:47 +0200

    cosas de clase

--- 04_formularios/maximo.php
+++ 04_formularios/maximo.php
@@ -14,5 +14,37 @@
         CREAR UN FORMULARIO DONDE SE INTENTE INTRODUCIR EL MÁXIMO VALOR Y SE
         COMPRUEBE SI HAS ACERTADO
     -->
+    <?php
+    $numeros = [1,5,3,9,20,15,22,11];
+
+    for($i = 0; $i < count($numeros); $i++) {
+        echo "$numeros[$i] ";
+    }
+    ?>
+    <br><br>
+    <form action="" method="post">
+        <label for="numero">Máximo</label>
+        <input type="text" name="numero" id="numero" placeholder="Introduce el máximo">
+        <br><br>
+        <input type="submit" value="Comprobar">
+    </form>
+
+    <?php
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $numero = $_POST["numero"];
+        $candidato = $numeros[0];
+
+        for($i = 1; $i < count($numeros); $i++) {
+            if($numeros[$i] > $candidato) $candidato = $numeros[$i];
+        }
+        $maximo = $candidato;
+
+        if($numero == $maximo) {
+            echo "<h1>¡Has acertado! El máximo es $numero</h1>";
+        } else {
+            echo "<h1>¡Fallaste! El máximo es $maximo</h1>";
+        }
+    }
+    ?>
 </body>
 </html>
---
+++ 04_formularios/tabla_de_multiplicar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de multiplicar</title>
+</head>
+<body>
+    <!--
+        Crear un formulario que reciba un número. Se mostrará en una tabla
+        HTML la tabla de multiplicar de ese número. Ejemplo:
+
+        3 x 1 = 3
+        3 x 2 = 6
+        3 x 3 = 9
+        ...
+        3 x 10 = 30
+    -->
+</body>
+</html>
