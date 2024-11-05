2c5f60be84fda5e63a9770f845046806c300254c
Alesa95 <alejandra_santiago_95@hotmail.com>
2024-11-05 14:18:18 +0100

    cositas acabadas

--- 04_formularios/videojuegos.php
+++ 04_formularios/videojuegos.php
@@ -27,14 +27,81 @@
 
             if($tmp_titulo == '') {
                 $err_titulo = "El título es obligatorio";
+            } else {
+                if(strlen($tmp_titulo) > 60) {
+                    $err_titulo = "El título debe tener entre 1 y 60 caracteres";
+                } else {
+                    $patron = "/^[a-zA-Z0-9 ]+$/";
+                    if(!preg_match($patron, $tmp_titulo)) {
+                        $err_titulo = "El título solo puede contener 
+                            letras, números y espacios en blanco";
+                    } else {
+                        $titulo = $tmp_titulo;
+                    }
+                }
             }
 
             if($tmp_consola == '') {
                 $err_consola = "La consola es obligatoria";
+            } else {
+                $consolas_validas = ["ps4","ps5","switch","xboxsx","pc"];
+                if(!in_array($tmp_consola, $consolas_validas)) {
+                    $err_consola = "Elige una consola válida";
+                } else {
+
+                }
+            }
+
+            if(strlen($tmp_descripcion) > 255) {
+                $err_descripcion = "La descripción no puede tener más de 255 caracteres";
+            } else {
+                $patron = "/^[a-zA-Z0-9 .,]+$/";
+                if(!preg_match($patron, $tmp_descripcion)) {
+                    $err_descripcion = "La descripción solo puede contener 
+                        letras, números, espacios en blanco, puntos y comas";
+                } else {
+                    $descripcion = $tmp_descripcion;
+                }
             }
 
             if($tmp_fecha_lanzamiento == '') {
                 $err_fecha_lanzamiento = "La fecha de lanzamiento es obligatoria";
+            } else {
+                $patron = "/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/";
+                if(!preg_match($patron,$tmp_fecha_lanzamiento)) {
+                    $err_fecha_lanzamiento = "El formato de la fecha es incorrecto";
+                } else {
+                    list($anno,$mes,$dia) = explode('-',$tmp_fecha_lanzamiento);
+                    if($anno < 1947) {
+                        $err_fecha_lanzamiento = "El año no puede ser anterior a 1947";
+                    } else {
+                        $anno_actual = date("Y");
+                        $mes_actual = date("m");
+                        $dia_actual = date("d");
+                        if($anno - $anno_actual > 10) {
+                            $err_fecha_lanzamiento = "El videojuego 
+                                no puede lanzarse dentro de más de 10 años";
+                        } elseif($anno - $anno_actual < 10) {
+                            //  FECHA VÁLIDA
+                            $fecha_lanzamiento = $tmp_fecha_lanzamiento;
+                        } else {
+                            if($mes - $mes_actual < 0) {
+                                //  FECHA VÁLIDA
+                                $fecha_lanzamiento = $tmp_fecha_lanzamiento;
+                            } elseif($mes - $mes_actual > 0) {
+                                $err_fecha_lanzamiento = "El videojuego 
+                                    no puede lanzarse dentro de más de 10 años";
+                            } else {
+                                if($dia - $dia_actual > 0) {
+                                    $err_fecha_lanzamiento = "El videojuego 
+                                        no puede lanzarse dentro de más de 10 años";
+                                } elseif($dia - $dia_actual <= 0) {
+                                    $fecha_lanzamiento = $tmp_fecha_lanzamiento;
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
         ?>
@@ -67,14 +134,17 @@
                     <input class="form-check-input" type="radio" name="consola" value="pc">
                     <label class="form-check-label">PC</label>
                 </div>
+                <?php if(isset($err_consola)) echo "<span class='error'>$err_consola</span>" ?>
             </div>
             <div class="mb-3">
                 <label class="form-label">Descripción</label>
                 <textarea class="form-control" name="descripcion"></textarea>
+                <?php if(isset($err_descripcion)) echo "<span class='error'>$err_descripcion</span>" ?>
             </div>
             <div class="mb-3">
                 <label class="form-label">Fecha de lanzamiento</label>
                 <input class="form-control" name="fecha_lanzamiento" type="date">
+                <?php if(isset($err_fecha_lanzamiento)) echo "<span class='error'>$err_fecha_lanzamiento</span>" ?>
             </div>
             <div class="mb-3">
                 <input class="btn btn-primary" type="submit" value="Enviar">
