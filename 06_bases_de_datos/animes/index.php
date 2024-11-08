6010bc98790e2bfcb697f09bb636049982946fcf
Alesa95 <alejandra_santiago_95@hotmail.com>
2024-11-08 10:59:31 +0100

    tabla con animes hecha

---
+++ 06_bases_de_datos/animes/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php
+        error_reporting( E_ALL );
+        ini_set( "display_errors", 1 ); 
+        
+        require('../conexion.php');
+    ?>
+</head>
+<body>
+    <div class="container">
+        <?php
+            $sql = "SELECT * FROM animes";
+            $resultado = $_conexion -> query($sql);
+        ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>Título</th>
+                    <th>Estudio</th>
+                    <th>Año</th>
+                    <th>Número de temporadas</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    while($fila = $resultado -> fetch_assoc()) {
+                        // ["titulo"=>"Frieren","nombre_estudio"="Pierrot"...]
+                        echo "<tr>";
+                        echo "<td>" . $fila["titulo"] . "</td>";
+                        echo "<td>" . $fila["nombre_estudio"] . "</td>";
+                        echo "<td>" . $fila["anno_estreno"] . "</td>";
+                        echo "<td>" . $fila["num_temporadas"] . "</td>";
+                        echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
