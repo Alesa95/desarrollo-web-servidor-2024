d3e61d105ac78a6be1d0585d226d49692aa350b7
Alesa95 <alejandra_santiago_95@hotmail.com>
2025-01-14 13:17:28 +0100

    api

--- 06_bases_de_datos/animes/api/api_estudios.php
+++ 06_bases_de_datos/animes/api/api_estudios.php
@@ -1,8 +1,6 @@
 <?php
-    /*
     error_reporting( E_ALL );
     ini_set( "display_errors", 1 );
-    */
 
     header("Content-type: application/json");
     include("conexion_pdo.php");
@@ -28,11 +26,27 @@
     }
 
     function manejarGet($_conexion) {
-        echo json_encode(["metodo" => "get"]);
+        $sql = "SELECT * FROM estudios";
+        $stmt = $_conexion -> prepare($sql);
+        $stmt -> execute();
+        $resultado = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($resultado);
     }
 
     function manejarPost($_conexion, $entrada) {
-        echo json_encode(["metodo" => "post"]);
+        $sql = "INSERT INTO estudios (nombre_estudio, ciudad, anno_fundacion) 
+            VALUES (:nombre_estudio, :ciudad, :anno_fundacion)";
+        $stmt = $_conexion -> prepare($sql);
+        $stmt -> execute([
+            "nombre_estudio" => $entrada["nombre_estudio"],
+            "ciudad" => $entrada["ciudad"],
+            "anno_fundacion" => $entrada["anno_fundacion"]
+        ]);
+        if($stmt) {
+            echo json_encode(["mensaje" => "el estudio se ha creado"]);
+        } else {
+            echo json_encode(["mensaje" => "error al crear el estudio"]);
+        }
     }
 
     function manejarPut($_conexion, $entrada) {
